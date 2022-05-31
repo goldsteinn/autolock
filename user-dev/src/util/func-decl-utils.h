c90c6d7735a0c78ebc2c7989e4b7e58ae77b5c67
Noah Goldstein <goldstein.w.n@gmail.com>
2022-05-30 19:59:55 -0500

    QOL Improvement for listing functions and add what we want to micro

--- user-dev/src/util/func-decl-utils.h
+++ user-dev/src/util/func-decl-utils.h
@@ -4,6 +4,10 @@
 #include "util/func-decl-types.h"
 
 void list_decls(decl_list_t const * decl_list);
+void list_decls_filtered(decl_list_t const * decl_list,
+                         char * restrict const * restrict decls_to_list,
+                         uint64_t ndecls_to_list);
+
 void run_decls(decl_list_t const * restrict decl_list,
                char * restrict const * restrict decls_to_run,
                uint64_t         ndecls_to_run,
